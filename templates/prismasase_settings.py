c78a3cf9526e212b047122a18ea3f77472d4f7de
Tanushree <34695939+ktanushree@users.noreply.github.com>
2024-08-08 16:38:53 -0400

    Update prismasase_settings.py

--- templates/prismasase_settings.py
+++ templates/prismasase_settings.py
@@ -1,6 +1,6 @@
 ######################################################
 # Service Account
 ######################################################
-PRISMASASE_CLIENT_ID="spov-automation@1908651310.iam.panserviceaccount.com"
-PRISMASASE_CLIENT_SECRET="6342177e-093b-461f-8c3d-11920bbda2ef"
-PRISMASASE_TSG_ID="1908651310"
+PRISMASASE_CLIENT_ID="paste client ID"
+PRISMASASE_CLIENT_SECRET="paste client secret"
+PRISMASASE_TSG_ID="paste TSG ID"
