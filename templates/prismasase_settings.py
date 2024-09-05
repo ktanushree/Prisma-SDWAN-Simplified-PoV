bdbf1d1bfcde22877037d234362655025d5b134b
Tanushree <34695939+ktanushree@users.noreply.github.com>
2024-09-05 09:39:09 -0400

    Delete templates directory

--- templates/prismasase_settings.py
+++
@@ -1,6 +0,0 @@
-######################################################
-# Service Account
-######################################################
-PRISMASASE_CLIENT_ID="paste client ID"
-PRISMASASE_CLIENT_SECRET="paste client secret"
-PRISMASASE_TSG_ID="paste TSG ID"
